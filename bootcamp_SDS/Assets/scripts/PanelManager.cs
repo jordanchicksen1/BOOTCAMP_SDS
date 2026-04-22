8b91224840cf45a635112c5a4a0a002ac4943cca
jordanchicksen1 <2333640@students.wits.ac.za>
2026-04-22 21:41:56 +0200

    its working mostly

--- bootcamp_SDS/Assets/scripts/PanelManager.cs
+++ bootcamp_SDS/Assets/scripts/PanelManager.cs
@@ -4,7 +4,15 @@ public class PanelManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject nextPanel, currentPanel, previousPanel;
-   
+    public GameObject scrollView;
+    public GameObject indoorAloneScroll;
+    public GameObject outdoorAloneScroll;
+    public GameObject indoorNotAloneScroll;
+    public GameObject outdoorNotAloneScroll;
+    public GameObject indoorNotAlonePanel;
+    public GameObject outdoorNotAlonePanel;
+    public GameObject indoorAlonePanel;
+    public GameObject outdoorAlonePanel;
 
     public void SelectPanel()
     {
@@ -16,5 +24,50 @@ public class PanelManager : MonoBehaviour
     {
         previousPanel.SetActive(true);
         currentPanel.SetActive(false);
+        scrollView.SetActive(false);
+        indoorAloneScroll.SetActive(false);
+        outdoorAloneScroll.SetActive(false);
+        indoorNotAloneScroll.SetActive(false);
+        outdoorNotAloneScroll.SetActive(false);
+    }
+
+    public void ShowIndoorAloneActivites()
+    {
+        indoorAlonePanel.SetActive(false);
+        scrollView.SetActive(true);
+        indoorAloneScroll.SetActive(true);
+        outdoorAloneScroll.SetActive(false);
+        indoorNotAloneScroll.SetActive(false);
+        outdoorNotAloneScroll.SetActive(false);
+    }
+
+    public void ShowOutdoorAloneActivites()
+    {
+        outdoorAlonePanel.SetActive(false);
+        scrollView.SetActive(true);
+        indoorAloneScroll.SetActive(false);
+        outdoorAloneScroll.SetActive(true);
+        indoorNotAloneScroll.SetActive(false);
+        outdoorNotAloneScroll.SetActive(false);
+    }
+
+    public void ShowIndoorNotAloneActivites()
+    {
+        indoorNotAlonePanel.SetActive(false);
+        scrollView.SetActive(true);
+        indoorAloneScroll.SetActive(false);
+        outdoorAloneScroll.SetActive(false);
+        indoorNotAloneScroll.SetActive(true);
+        outdoorNotAloneScroll.SetActive(false);
+    }
+
+    public void ShowOutdoorNotAloneActivites()
+    {
+        outdoorNotAlonePanel.SetActive(false);
+        scrollView.SetActive(true);
+        indoorAloneScroll.SetActive(false);
+        outdoorAloneScroll.SetActive(false);
+        indoorNotAloneScroll.SetActive(false);
+        outdoorNotAloneScroll.SetActive(true);
     }
 }
