0da87a403a7f71dfa1bcec9dd00675c701b345d9
RamzCas <2749414@students.wits.ac.za>
2026-04-22 20:41:18 +0200

    music manager made

---
+++ bootcamp_SDS/Assets/scripts/MusicManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    public AudioSource Source;
+
+    [Header("Audio CLips")]
+    public AudioClip ActivitySelected;
+}
